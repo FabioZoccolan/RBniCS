4bdad1a1eac0b8d3d3aabc6dcc5a9649bf8c5e5b
Fabio Zoccolan <fabiozoccolan11@gmail.com>
2021-08-27 19:06:06 +0200

    UQ Unsteady Square, Graetz, GraetzGeom

---
+++ tutorials/Thesis/UQ/Unsteady/Graetz/Graetz_Geom/reduction_methods/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2015-2021 by the RBniCS authors
+#
+# This file is part of RBniCS.
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from .online_rectification_decorated_reduction_method import OnlineRectificationDecoratedReductionMethod
+from .online_stabilization_decorated_reduction_method import OnlineStabilizationDecoratedReductionMethod
+from .online_vanishing_viscosity_decorated_reduction_method import OnlineVanishingViscosityDecoratedReductionMethod
+from .weighted_uncertainty_quantification_decorated_reduction_method import WeightedUncertaintyQuantificationDecoratedReductionMethod
+
+__all__ = [    
+    'WeightedUncertaintyQuantificationDecoratedReductionMethod',
+    "OnlineRectificationDecoratedReductionMethod",
+    "OnlineStabilizationDecoratedReductionMethod",
+    "OnlineVanishingViscosityDecoratedReductionMethod"
+]
